af9ad61563e32786f44ca1515fa8c8641fe0c54f
Oyinda David <oyindadavid18@gmail.com>
2021-06-23 15:16:34 +0100

    setup allcourses and course route

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"log"
-	"github.com/gorilla/mux"
 	"net/http"
+
+	"github.com/ethereum/go-ethereum/params"
+	"github.com/gorilla/mux"
 )
 
 
@@ -12,11 +14,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to the Courses API")
 }
 
+func allCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "List of all courses")
+}
+
+func course(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	fmt.Fprintf(w, "This is " + params["courseid"])
+}
+
 
 func main() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/", home)
+	router.HandleFunc("/api/v1", home)
+	router.HandleFunc("/api/v1/courses", allCourses)
+	router.HandleFunc("/api/v1/courses/{courseid}", course)
 
 	fmt.Println("Listening at port 5000")
 	log.Fatal(http.ListenAndServe(":5000", router))
