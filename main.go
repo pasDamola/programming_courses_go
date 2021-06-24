ac8a55435fe4564adc44e9d28a48e891a1f25e8b
Oyinda David <oyindadavid18@gmail.com>
2021-06-24 10:20:30 +0100

    Updating a course information with id

--- main.go
+++ main.go
@@ -66,6 +66,36 @@ func course(w http.ResponseWriter, r *http.Request) {
 				w.Write([]byte("422 - Please supply course information " + "in JSON format"))
 			}
 		}
+
+		//---PUT is for creating or updating
+		// existing course---
+		if r.Method == "PUT" {
+			var newCourse courseInfo
+			reqBody, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				json.Unmarshal(reqBody, &newCourse)
+				if newCourse.Title == "" {
+					w.WriteHeader(
+						http.StatusUnprocessableEntity)
+					w.Write([]byte("422 - Please supply course " + " information " + "in JSON format"))
+					return
+				}
+				// check if course exists; add only if
+				// course does not exist
+				if _, ok := courses[params["courseid"]]; !ok {
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusCreated)
+					w.Write([]byte("201 - Course added: " + params["courseid"]))
+				} else {
+					// update course
+					courses[params["courseid"]] = newCourse
+					w.WriteHeader(http.StatusNoContent)
+				}
+			} else {
+				w.WriteHeader(http.StatusUnprocessableEntity)
+				w.Write([]byte("422 - Please supply " + "course information " + "in JSON format"))
+			}
+		}
 	}
 }
 
